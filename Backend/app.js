be4313b811d7d69749aa69bd349049a2dd1c6fbe
eliekhazzaka <eliekhazzaka90@gmail.com>
2022-07-25 10:52:47 +0300

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require("./routes/book-routes");
+
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/books", router);
+
+
+mongoose.connect("mongodb+srv://admin:5v9dVLH2fRldIBYv@cluster0.4o4ujtc.mongodb.net/?retryWrites=true&w=majority"
+).then(()=>console.log("Connected to database"))
+.then(()=>{
+    app.listen(5000)
+
+}).catch((err) => console.log(err));
+
